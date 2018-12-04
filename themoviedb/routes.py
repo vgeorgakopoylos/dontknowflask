238bc89b4969a7ab758223a45b65d35463143d3a
vgeorgakopoylos <v.georgakopoylos@gmai.com>
2018-12-04 17:20:06 +0200

    modified:   themoviedb/__pycache__/routes.cpython-37.pyc modified:   themoviedb/__pycache__/transformations.cpython-37.pyc modified:   themoviedb/routes.py modified:   themoviedb/transformations.py

--- themoviedb/routes.py
+++ themoviedb/routes.py
@@ -13,7 +13,7 @@ def moviesupcoming():
 
 	dataResp = requestlib.apiCallGeneral(searchItem='movie', searchMethod='upcoming',additionalParams={"page" : curPage});
 	upcomingMovies = dataResp['results'];
-	upcomingMovies=transformations.upcomingMoviesTransf(upcomingMovies)
+	upcomingMovies=transformations.appendPosterPath(upcomingMovies)
 	totalPages = dataResp['total_pages'];
 
 	return render_template('itemlist.html',itemList = upcomingMovies, 
@@ -24,6 +24,42 @@ def moviesupcoming():
 										   pageLimit = 10,
 										   pageColLimit = 3,
 										   totalPages = totalPages);
+										   
+@themoviedb.route('/moviestoprated')
+def moviestoprated():
+	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
+
+	dataResp = requestlib.apiCallGeneral(searchItem='movie', searchMethod='top_rated',additionalParams={"page" : curPage});
+	topRatedMovies = dataResp['results'];
+	topRatedMovies=transformations.appendPosterPath(topRatedMovies)
+	totalPages = dataResp['total_pages'];
+
+	return render_template('itemlist.html',itemList = topRatedMovies, 
+										   maxNumOfCol = 4, 
+										   curPage = curPage,
+										   drilldownMethod = 'moviedesc',
+										   pagingMethod = 'moviestoprated',
+										   pageLimit = 10,
+										   pageColLimit = 3,
+										   totalPages = totalPages);		
+
+@themoviedb.route('/moviespopular')
+def moviespopular():
+	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
+
+	dataResp = requestlib.apiCallGeneral(searchItem='movie', searchMethod='popular',additionalParams={"page" : curPage});
+	topRatedMovies = dataResp['results'];
+	topRatedMovies=transformations.appendPosterPath(topRatedMovies)
+	totalPages = dataResp['total_pages'];
+
+	return render_template('itemlist.html',itemList = topRatedMovies, 
+										   maxNumOfCol = 4, 
+										   curPage = curPage,
+										   drilldownMethod = 'moviedesc',
+										   pagingMethod = 'moviespopular',
+										   pageLimit = 10,
+										   pageColLimit = 3,
+										   totalPages = totalPages);	
 	
 @themoviedb.route('/favicon.ico')
 def favicon():
--- themoviedb/transformations.py
+++ themoviedb/transformations.py
@@ -2,13 +2,13 @@ import json
 from flask import current_app
 from flask import url_for
 
-def upcomingMoviesTransf(relatedMovies):
-	for relatedMovie in relatedMovies:
-		if relatedMovie['poster_path'] is None:
-			relatedMovie['poster_dom'] = '<div class="nopostercont"><img src="'+url_for('static', filename='images/noposter.jpg')+'"><div class="nopostercentered">'+relatedMovie['title']+'</div></div>';
+def appendPosterPath(items):
+	for item in items:
+		if item['poster_path'] is None:
+			item['poster_dom'] = '<div class="nopostercont"><img src="'+url_for('static', filename='images/noposter.jpg')+'"><div class="nopostercentered">'+item['title']+'</div></div>';
 		else:
-			relatedMovie['poster_dom'] = '<img src="'+current_app.config['IMAGE_URL'] + current_app.config['IMAGE_WIDTH_GENERAL']+relatedMovie['poster_path']+'">';
+			item['poster_dom'] = '<img src="'+current_app.config['IMAGE_URL'] + current_app.config['IMAGE_WIDTH_GENERAL']+item['poster_path']+'">';
 			
-	return relatedMovies
+	return items;
 	
 	
