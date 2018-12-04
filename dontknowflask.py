17c6909bb01bf037f95353939553aa3475e3ea16
vgeorgakopoylos <v.georgakopoylos@gmai.com>
2018-12-04 11:35:44 +0200

    modified:   dontknowflask.py modified:   themoviedb/__init__.py

--- dontknowflask.py
+++ dontknowflask.py
@@ -1,1 +1,2 @@
 from themoviedb import themoviedb
+
--- themoviedb/__init__.py
+++ themoviedb/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import os
+from os import environ
 
 themoviedb = Flask(__name__)
 
@@ -13,3 +14,5 @@ themoviedb.templates_auto_reload = True
 themoviedb.config.from_pyfile(os.path.join(os.path.dirname(__file__), 'static','configuration','devconfig.cfg'));
 
 
+if __name__ == 'themoviedb':
+	themoviedb.run(port= environ.get("PORT", 5000))
