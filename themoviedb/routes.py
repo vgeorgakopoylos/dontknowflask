e90bbb80c0ce11a8ac53ad00690ec4b3620f4bba
vgeorgakopoylos <v.georgakopoylos@gmai.com>
2018-12-14 11:38:24 +0200

    modified:   themoviedb/routes.py modified:   themoviedb/static/configuration/devconfig.cfg modified:   themoviedb/static/css/styles.css modified:   themoviedb/templates/imageslideshow.html modified:   themoviedb/templates/itemdescription.html modified:   themoviedb/templates/itemoverallinfo.html modified:   themoviedb/templates/videoslideshow.html modified:   themoviedb/transformations.py themoviedb/static/fonts/KR ReadMe.txt themoviedb/static/fonts/KRAction.ttf themoviedb/templates/casting.html themoviedb/templates/directors.html

--- themoviedb/routes.py
+++ themoviedb/routes.py
@@ -26,7 +26,7 @@ def moviesupcoming():
 	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
 	dataResp = requestlib.apiTMDBCallGeneral(searchItem='movie', searchMethod='upcoming',additionalParams={"page" : curPage});
 	upcomingMovies = dataResp['results'];
-	upcomingMovies=transformations.appendPosterPathGeneral(upcomingMovies)
+	upcomingMovies = transformations.appendImagePath (upcomingMovies, '', 'image_path', 'IMAGE_WIDTH_GENERAL', 'poster_path');
 	totalPages = dataResp['total_pages'];
 	return render_template('itemlist.html',itemList = upcomingMovies, 
 										   maxNumOfCol = 4, 
@@ -43,7 +43,7 @@ def moviestoprated():
 
 	dataResp = requestlib.apiTMDBCallGeneral(searchItem='movie', searchMethod='top_rated',additionalParams={"page" : curPage});
 	topRatedMovies = dataResp['results'];
-	topRatedMovies=transformations.appendPosterPathGeneral(topRatedMovies)
+	topRatedMovies = transformations.appendImagePath (topRatedMovies, '', 'image_path', 'IMAGE_WIDTH_GENERAL', 'poster_path');
 	totalPages = dataResp['total_pages'];
 
 	return render_template('itemlist.html',itemList = topRatedMovies, 
@@ -60,11 +60,11 @@ def moviespopular():
 	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
 
 	dataResp = requestlib.apiTMDBCallGeneral(searchItem='movie', searchMethod='popular',additionalParams={"page" : curPage});
-	topRatedMovies = dataResp['results'];
-	topRatedMovies=transformations.appendPosterPathGeneral(topRatedMovies);
+	topPopular = dataResp['results'];
+	topPopular = transformations.appendImagePath (topPopular, '', 'image_path', 'IMAGE_WIDTH_GENERAL', 'poster_path');
 	totalPages = dataResp['total_pages'];
 
-	return render_template('itemlist.html',itemList = topRatedMovies, 
+	return render_template('itemlist.html',itemList = topPopular, 
 										   maxNumOfCol = 4, 
 										   curPage = curPage,
 										   drilldownMethod = 'moviedesc',
@@ -76,9 +76,14 @@ def moviespopular():
 @themoviedb.route('/moviedesc')
 def moviedesc():
 	movieInfo = requestlib.apiTMDBCallById(searchItem='movie', itemId = request.args.get('itemid'), itemSuffix='',additionalParams={'append_to_response':'images,videos','include_image_language':session['languages']});
-	movieInfo = transformations.appendPosterPathDetails(movieInfo);
-
-	return render_template('itemoverallinfo.html',itemInfo = movieInfo);	
+	movieInfo = transformations.appendImagePath (movieInfo, 'images.posters;images.backdrops', 'image_path;image_path', 'IMAGE_WIDTH_DETAIL;IMAGE_WIDTH_DETAIL', 'file_path;file_path');
+	movieInfo = transformations.appendVideoPath (movieInfo, 'videos.results', 'video_path', 'site', 'key');
+	
+	credits = requestlib.apiTMDBCallById(searchItem='movie', itemId = request.args.get('itemid'), itemSuffix='credits');
+	credits = transformations.appendImagePath (credits, 'cast;crew', 'image_path;image_path', 'IMAGE_WIDTH_LOGO;IMAGE_WIDTH_LOGO', 'profile_path;profile_path');
+	
+	return render_template('itemoverallinfo.html',itemInfo = movieInfo, credits = credits);	
+	
 
 @themoviedb.route('/favicon.ico')
 def favicon():
--- themoviedb/transformations.py
+++ themoviedb/transformations.py
@@ -2,13 +2,15 @@ import json
 from flask import current_app
 from flask import url_for
 
+#old function replaced by appendImagePath
 def appendPosterPathGeneral(items):
 	for item in items:
 		if item['poster_path'] is not None:
 			item['image_path'] = current_app.config['IMAGE_URL'] + current_app.config['IMAGE_WIDTH_GENERAL']+item['poster_path'];
-			
+		
 	return items;
 	
+#old function replaced by appendImagePath and 	appendVideoPath
 def appendPosterPathDetails(itemInfo):
 	for poster in itemInfo['images']['posters']:
 		if poster['file_path'] is not None:
@@ -20,6 +22,53 @@ def appendPosterPathDetails(itemInfo):
 			
 	for video in itemInfo['videos']['results']:
 		if video['site'].lower() == "youtube":
-			video['video_path'] = current_app.config['VIDEO_PREFIX']['YOUTUBE']+video['key'];			
+			video['video_path'] = current_app.config['VIDEO_PREFIX']['YOUTUBE']+video['key'];	
 	
 	return itemInfo;
+
+def appendImagePath (items, loopPaths, appendedKey, imageWidth, targetKeyValue):
+	loopPathsSplitted = loopPaths.split(';');
+	appendedKeySplitted = appendedKey.split(';');
+	imageWidthSplitted = imageWidth.split(';');
+	targetKeyValueSplitted = targetKeyValue.split(';');
+	idx = 0;
+	for loopPath in loopPathsSplitted:
+		tempLoopPath = '';
+		
+		if loopPath != '':
+			for loopKey in loopPath.split('.'):
+				tempLoopPath =  tempLoopPath + '[\''+loopKey+'\']';	
+				
+		codeBlock = '''for item in items'''+tempLoopPath+''': 
+						if item[\''''+targetKeyValueSplitted[idx]+'''\'] is not None: 
+							item[\''''+appendedKeySplitted[idx]+'''\'] = current_app.config[\'IMAGE_URL\'] + current_app.config[\''''+imageWidthSplitted[idx]+'''\'] + item[\''''+targetKeyValueSplitted[idx]+'''\'];'''
+		execCodeObject = compile(codeBlock ,'<string>', 'exec');				
+		exec(execCodeObject);
+		idx += 1;
+	return items;
+	
+def appendVideoPath (items, loopPaths, appendedKey, siteKey, targetKeyValue):	
+	loopPathsSplitted = loopPaths.split(';');
+	appendedKeySplitted = appendedKey.split(';');
+	siteKeySplitted = siteKey.split(';');
+	targetKeyValueSplitted = targetKeyValue.split(';');
+	idx = 0;
+	for loopPath in loopPathsSplitted:
+		tempLoopPath = '';
+		
+		if loopPath != '':
+			for loopKey in loopPath.split('.'):
+				tempLoopPath =  tempLoopPath + '[\''+loopKey+'\']';	
+
+		codeBlock = '''for item in items'''+tempLoopPath+''': 
+						if item[\''''+siteKeySplitted[idx]+'''\'].lower() == \'youtube\': 
+							item[\''''+appendedKeySplitted[idx]+'''\'] = current_app.config[\'VIDEO_PREFIX\'][\'YOUTUBE\']+item[\''''+targetKeyValueSplitted[idx]+'''\'];''';
+		execCodeObject = compile(codeBlock ,'<string>', 'exec')				
+		exec(execCodeObject);
+		idx += 1;
+	return items;	
+	
+	
+	
+	
+	
