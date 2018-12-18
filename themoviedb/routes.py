1a77c134c56a196d748b1f9a8e7b850e5cbc4e27
vgeorgakopoylos <v.georgakopoylos@gmai.com>
2018-12-18 10:07:52 +0200

    modified:   themoviedb/__pycache__/routes.cpython-37.pyc modified:   themoviedb/routes.py modified:   themoviedb/static/configuration/devconfig.cfg modified:   themoviedb/static/css/styles.css new file:   themoviedb/static/js/asyncCallEvents.js modified:   themoviedb/templates/casting.html modified:   themoviedb/templates/directors.html modified:   themoviedb/templates/imageslideshow.html modified:   themoviedb/templates/index.html modified:   themoviedb/templates/itemdescription.html modified:   themoviedb/templates/itemlist.html modified:   themoviedb/templates/itemoverallinfo.html modified:   themoviedb/templates/paging.html modified:   themoviedb/templates/ratingfivestar.html new file:   themoviedb/templates/similaritemshorizontal.html modified:   themoviedb/templates/videoslideshow.html

--- themoviedb/routes.py
+++ themoviedb/routes.py
@@ -5,7 +5,6 @@ import os
 from flask import jsonify
 import datetime
 
-
 @themoviedb.before_request
 def set_parameters():
 	if 'languages' not in session:
@@ -19,6 +18,7 @@ def set_parameters():
 			session['languages'] = 'en'
 		session['languages'] = session['languages'] + 'null';
 
+################################Movies Start################################		
 @themoviedb.route('/')
 @themoviedb.route('/home')
 @themoviedb.route('/moviesupcoming')
@@ -69,12 +69,14 @@ def moviespopular():
 										   curPage = curPage,
 										   drilldownMethod = 'moviedesc',
 										   pagingMethod = 'moviespopular',
-										   pageLimit = 10,
+										   pageLimit = 9,
 										   pageColLimit = 3,
 										   totalPages = totalPages);	
 										   
 @themoviedb.route('/moviedesc')
 def moviedesc():
+	curPage = 1 if request.args.get('simMoviescurPage') is None else request.args.get('simMoviescurPage');
+	
 	movieInfo = requestlib.apiTMDBCallById(searchItem='movie', itemId = request.args.get('itemid'), itemSuffix='',additionalParams={'append_to_response':'images,videos','include_image_language':session['languages']});
 	movieInfo = transformations.appendImagePath (movieInfo, 'images.posters;images.backdrops', 'image_path;image_path', 'IMAGE_WIDTH_DETAIL;IMAGE_WIDTH_DETAIL', 'file_path;file_path');
 	movieInfo = transformations.appendVideoPath (movieInfo, 'videos.results', 'video_path', 'site', 'key');
@@ -82,9 +84,146 @@ def moviedesc():
 	credits = requestlib.apiTMDBCallById(searchItem='movie', itemId = request.args.get('itemid'), itemSuffix='credits');
 	credits = transformations.appendImagePath (credits, 'cast;crew', 'image_path;image_path', 'IMAGE_WIDTH_LOGO;IMAGE_WIDTH_LOGO', 'profile_path;profile_path');
 	
-	return render_template('itemoverallinfo.html',itemInfo = movieInfo, credits = credits);	
-	
+	similarResp = requestlib.apiTMDBCallById(searchItem='movie', itemId = request.args.get('itemid'), itemSuffix='similar');
+	totalPages = similarResp['total_pages'];
+	similar = similarResp['results'];
+	similar = transformations.appendImagePath (similar, '', 'image_path', 'IMAGE_WIDTH_SIMILAR', 'poster_path');
 
+	return render_template('itemoverallinfo.html',	itemInfo = movieInfo,
+													credits = credits, 
+													similarItems = similar,
+													similarMethod = 'moviedesc',
+													pagingMethod = 'similarMoviePage',
+													pageParameterName = 'simMoviescurPage',
+													pagingGenParameters = {'itemid':request.args.get('itemid')},
+													curPage = curPage,
+													pageLimit = 10,
+													totalPages = totalPages);	
+
+@themoviedb.route('/similarMoviePage')		
+def similarMoviePage():
+	curPage = 1 if request.args.get('simMoviescurPage') is None else request.args.get('simMoviescurPage');
+
+	similarResp = requestlib.apiTMDBCallById(searchItem='movie', itemId = request.args.get('itemid'), itemSuffix='similar', additionalParams = {'page':curPage});
+	totalPages = similarResp['total_pages'];
+	similar = similarResp['results'];
+	similar = transformations.appendImagePath (similar, '', 'image_path', 'IMAGE_WIDTH_SIMILAR', 'poster_path');	
+	
+	return render_template('similaritemshorizontal.html',
+										similarItems = similar,
+										similarMethod = 'moviedesc',
+										pagingMethod = 'similarMoviePage',
+										pageParameterName = 'simMoviescurPage',
+										pagingGenParameters = {'itemid':request.args.get('itemid')},
+										curPage = curPage,
+										pageLimit = 10,
+										totalPages = totalPages);
+							
+
+################################Movies End################################
+
+################################TV Start################################
+@themoviedb.route('/tvonair')	
+def tvonair():			
+	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
+
+	dataResp = requestlib.apiTMDBCallGeneral(searchItem='tv', searchMethod='on_the_air',additionalParams={"page" : curPage});
+	tvonair = dataResp['results'];
+	tvonair = transformations.appendImagePath (tvonair, '', 'image_path', 'IMAGE_WIDTH_GENERAL', 'poster_path');
+	totalPages = dataResp['total_pages'];
+
+	return render_template('itemlist.html',itemList = tvonair, 
+										   maxNumOfCol = 4, 
+										   curPage = curPage,
+										   drilldownMethod = 'tvdesc',
+										   pagingMethod = 'tvonair',
+										   pageLimit = 9,
+										   pageColLimit = 3,
+										   totalPages = totalPages);		
+
+@themoviedb.route('/tvtoprated')
+def tvtoprated():
+	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
+
+	dataResp = requestlib.apiTMDBCallGeneral(searchItem='tv', searchMethod='top_rated',additionalParams={"page" : curPage});
+	tvtoprated = dataResp['results'];
+	tvtoprated = transformations.appendImagePath (tvtoprated, '', 'image_path', 'IMAGE_WIDTH_GENERAL', 'poster_path');
+	totalPages = dataResp['total_pages'];
+
+	return render_template('itemlist.html',itemList = tvtoprated, 
+										   maxNumOfCol = 4, 
+										   curPage = curPage,
+										   drilldownMethod = 'tvdesc',
+										   pagingMethod = 'tvtoprated',
+										   pageLimit = 9,
+										   pageColLimit = 3,
+										   totalPages = totalPages);										   
+	
+@themoviedb.route('/tvpopular')
+def tvpopular():
+	curPage = 1 if request.args.get('curPage') is None else request.args.get('curPage');
+
+	dataResp = requestlib.apiTMDBCallGeneral(searchItem='tv', searchMethod='top_rated',additionalParams={"page" : curPage});
+	tvpopular = dataResp['results'];
+	tvpopular = transformations.appendImagePath (tvpopular, '', 'image_path', 'IMAGE_WIDTH_GENERAL', 'poster_path');
+	totalPages = dataResp['total_pages'];
+
+	return render_template('itemlist.html',itemList = tvpopular, 
+										   maxNumOfCol = 4, 
+										   curPage = curPage,
+										   drilldownMethod = 'tvdesc',
+										   pagingMethod = 'tvpopular',
+										   pageLimit = 9,
+										   pageColLimit = 3,
+										   totalPages = totalPages);			
+	
+@themoviedb.route('/tvdesc')		
+def tvdesc():
+	curPage = 1 if request.args.get('simTVscurPage') is None else request.args.get('simTVscurPage');
+	
+	tvInfo = requestlib.apiTMDBCallById(searchItem='tv', itemId = request.args.get('itemid'), itemSuffix='',additionalParams={'append_to_response':'images,videos','include_image_language':session['languages']});
+	tvInfo = transformations.appendImagePath (tvInfo, 'images.posters;images.backdrops', 'image_path;image_path', 'IMAGE_WIDTH_DETAIL;IMAGE_WIDTH_DETAIL', 'file_path;file_path');
+	tvInfo = transformations.appendVideoPath (tvInfo, 'videos.results', 'video_path', 'site', 'key');
+	
+	credits = requestlib.apiTMDBCallById(searchItem='tv', itemId = request.args.get('itemid'), itemSuffix='credits');
+	credits = transformations.appendImagePath (credits, 'cast;crew', 'image_path;image_path', 'IMAGE_WIDTH_LOGO;IMAGE_WIDTH_LOGO', 'profile_path;profile_path');
+	
+	similarResp = requestlib.apiTMDBCallById(searchItem='tv', itemId = request.args.get('itemid'), itemSuffix='similar');
+	totalPages = similarResp['total_pages'];
+	similar = similarResp['results'];
+	similar = transformations.appendImagePath (similar, '', 'image_path', 'IMAGE_WIDTH_SIMILAR', 'poster_path');
+
+	return render_template('itemoverallinfo.html',	itemInfo = tvInfo,
+													credits = credits, 
+													similarItems = similar,
+													similarMethod = 'tvdesc',
+													pagingMethod = 'similarTVPage',
+													pageParameterName = 'simTVscurPage',
+													pagingGenParameters = {'itemid':request.args.get('itemid')},
+													curPage = curPage,
+													pageLimit = 10,
+													totalPages = totalPages);
+													
+@themoviedb.route('/similarTVPage')		
+def similarTVPage():
+	curPage = 1 if request.args.get('simTVcurPage') is None else request.args.get('simTVcurPage');
+
+	similarResp = requestlib.apiTMDBCallById(searchItem='tv', itemId = request.args.get('itemid'), itemSuffix='similar', additionalParams = {'page':curPage});
+	totalPages = similarResp['total_pages'];
+	similar = similarResp['results'];
+	similar = transformations.appendImagePath (similar, '', 'image_path', 'IMAGE_WIDTH_SIMILAR', 'poster_path');	
+	
+	return render_template('similaritemshorizontal.html',
+										similarItems = similar,
+										similarMethod = 'moviedesc',
+										pagingMethod = 'similarTVPage',
+										pageParameterName = 'simTVcurPage',
+										pagingGenParameters = {'itemid':request.args.get('itemid')},
+										curPage = curPage,
+										pageLimit = 10,
+										totalPages = totalPages);													
+		
+################################TV End################################		
 @themoviedb.route('/favicon.ico')
 def favicon():
 	return send_from_directory(os.path.join(themoviedb.root_path, 'static'),'images/favicon.ico')
